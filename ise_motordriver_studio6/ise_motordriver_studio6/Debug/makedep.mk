c815bbafade69deb09f46a5d5f5f2113aab0a84c
ip_sakura <tokati.yume@gmail.com>
2018-01-23 01:39:46 +0900

    pwm freq

---
+++ ise_motordriver_studio6/ise_motordriver_studio6/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+I2CSlave.cpp
+
+ise_motordriver_studio6.cpp
+
+motordriver.cpp
+
