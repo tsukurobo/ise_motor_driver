5aa5bc2c215b399c9c585bbda51ad19a1376d8f1
0x005c <52650857+0x005c@users.noreply.github.com>
2019-09-15 11:14:55 +0900

    Refactor motordriver.cpp

--- ise_motor_driver_ip/ise_motor_driver_ip/motordriver.cpp
+++ ise_motor_driver_ip/ise_motor_driver_ip/motordriver.cpp
@@ -11,42 +11,39 @@
 //20khz
 //10khz
 //1khz
-const int TOP=255;
+const int TOP = 255;
 enum{
-	TMR0Full = TOP,//255,
-	TMR0INI = 0, //122,
+  TMR0Full = TOP,//255,
+  TMR0INI = 0, //122,
 };
 
-int motor_init(void)
-{
-	//setup
-	DDRD |= 0b11100000;
-	
-	TCCR0A = 0b10100011;  //highspeedPWM
-	
-	TCCR0B = 0b00000010;  //1/8,Top=OVF
+void motor_init(void) {
+  //setup
+  DDRD |= 0b11100000;
+
+  TCCR0A = 0b10100011;  //highspeedPWM
+
+  TCCR0B = 0b00000010;  //1/8,Top=OVF
 }
 
-int pwm_map (int duty){
-	return TMR0INI + (int)(TOP/100*duty); //duty=0~100
+int pwm_map (int duty) {
+  return TMR0INI + (int)(TOP/100*duty); //duty=0~100
 }
-	
-	
-	
-int motor_set_speed (int power) {
-	
-     	OCR0A = pwm_map(abs(power));
-	    OCR0B = pwm_map(abs(power));
+  
+  
+  
+void motor_set_speed (int power) {
+  OCR0A = pwm_map(abs(power));
+  OCR0B = pwm_map(abs(power));
 
-		if (power < 0){
-			//PORTD = 0b01100000;	  //PHASE=high
-			PORTD &= 0b01111111;
-		}
-		
-		else if(power > 0){
-			//PORTD = 0b11100000;   //PHASE=low
-			PORTD |= 0b10000000;
-		}
-				
+  if (power < 0){
+    //PORTD = 0b01100000;    //PHASE=high
+    PORTD &= 0b01111111;
+  }
+
+  else if(power > 0){
+    //PORTD = 0b11100000;   //PHASE=low
+    PORTD |= 0b10000000;
+  }
 }
 
